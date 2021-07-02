6199766add47b97a87aedc5b8906b6c272d5cf5e
quanhoang288 <quanpro288@gmail.com>
2021-07-02 17:15:56 +0700

    update components and add new searchable input field

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,7 +2,7 @@
 <div class="{{$parentClass ?? 'col-sm-4 d-inline'}}">
     <select name="{{$name}}" id="{{$name}}" class="{{$selectClass ?? 'custom-select w-25'}}">
         @foreach ($options as $option)
-            <option value="{{$option}}">{{$option}}</option>
+            <option value="{{$option['value'] ?? $option}}">{{$option['display'] ?? $option}}</option>
         @endforeach
     </select>
 </div>
