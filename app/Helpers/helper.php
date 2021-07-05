5e89eb052c57fed2cabe624ee1aea5f9c3b3e981
trinhhuydat <huydat1508@gmail.com>
2021-07-05 09:42:35 +0700

    dat upcode 5.7.2021

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -15,38 +15,5 @@
 	    	return $html;
 		}
 	}
-// Danh mục đệ quy
-/**
-*$parent_id_child : parent_id của bản ghi đang sửa
-*/
-function getCategory($categories, $parent,$char,$parent_id_child) {
-	foreach ($categories as $key => $value) {
-		if($value['parent_id'] == $parent) {
-			if($value['id'] == $parent_id_child) {
-				echo '<option selected value="'.$value['id'].'">'.$char.$value['name'].'</option>';
-			}else{
-				echo '<option value="'.$value['id'].'">'.$char.$value['name'].'</option>';
-			}
-			$new_parent = $value['id'];
-			getCategory($categories,$new_parent,$char.'|--',$parent_id_child);
-		}
-	}
-}
-
-function showCategory($categories, $parent,$char) {
-	foreach ($categories as $key => $value) {
-		if($value['parent_id'] == $parent) {
-			$html  = '<div>';
-			$html .= '<div class="item-menu">';
-			$html .= '<span>'.$char.$value['name'].'</span>';
-			$html .= '<div class="category-fix">';
-			$html .= '<a class="btn-category btn-primary" href="'.route('category.edit',['id'=>$value['id']]).'"><i class="fa fa-edit"></i></a>';
-			$html .= '<a class="btn-category btn-danger" href="'.route('category.destroy',['id'=>$value['id']]).'"><i class="fas fa-times"></i></i></a></div></div>';
-			echo $html;
-			$new_parent = $value['id'];
-			showCategory($categories,$new_parent,$char.'|--');
-		}
-	}
-}
 
 ?>
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function list(){
-        $projects = Project::all();
+        $projects = Project::paginate(10);
         return view('site.project.project', compact('projects'));
     }
-    public function store(Request $request) {
+    public function store(ProjectRequest $request) {
         $project = new Project();
         $project->code = $request->project_code;
         $project->name = $request->project_name;
@@ -25,7 +26,7 @@ class ProjectController extends Controller
         $projects = Project::all();
         return view('site.project.edit-project', compact('project','projects'));
     }
-    public function update(Request $request, $id) {
+    public function update(ProjectRequest $request, $id) {
         $project = Project::findOrFail($id);
         $project->code = $request->project_code;
         $project->name = $request->project_name;
@@ -39,13 +40,12 @@ class ProjectController extends Controller
         $project->delete();
         return redirect()->route('project.list')->with('massage','Đã xóa dự án thành công!');
     }
-    public function action(Request $request){
-        if ($request->name == 'store'){
-            return redirect()->route('project.store');
-        }
-        if ($request->name == 'update'){
-            return redirect()->route('project.update');
-        }
-
-    }
+//    public function action(ProjectRequest $request){
+//        if ($request->name == 'store'){
+//            return redirect()->route('project.store');
+//        }
+//        if ($request->name == 'update'){
+//            return redirect()->route('project.update');
+//        }
+//    }
 }
