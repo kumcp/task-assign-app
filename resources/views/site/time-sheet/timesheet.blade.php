49075c181de38e0ebd93392023c7002d8f93675e
trinhhuydat <huydat1508@gmail.com>
2021-07-08 09:06:46 +0700

    show timesheet

--- resources/views/site/time-sheet/timesheet.blade.php
+++ resources/views/site/time-sheet/timesheet.blade.php
@@ -11,16 +11,12 @@
                             'label' => 'Tên công việc',
 
                         ])
-                        {{-- <div class="col-md-3  d-inline pl-0"><label for="job_name">Tên công việc</label></div>
-                        <input type="text" name="job_name" id="job_name"> --}}
                     </div>
                     <div class="form-group-row mb-3">
                         @include('components.input-text', [
                             'name' => 'assignee', 
                             'label' => 'Đối tượng xử lý'
                         ])
-                        {{-- <div class="col-md-3 d-inline pl-0"><label for="assignee">Đối tượng xử lý</label></div>
-                        <input type="text" name="assignee" id="assignee"> --}}
                         <label for="process_method" class="ml-5">(Hình thức xử lý)</label>
                     </div>
                     <div class="form-group-row mb-3">
@@ -34,12 +30,7 @@
                             'name' => 'to_date', 
                             'label' => 'Đến ngày',
                         ])
-                        {{-- <label for="from_date" class="col-form-label">Từ ngày</label>
-                        <input type="date" name="from_date" id="from_date">
-                        <label for="to_date" class="mx-5">Đến ngày</label>
-                        <input type="date" name="to_date" id="to_date"> --}}
                     </div>
-    
                     <div class="form-group-row mb-3">
                         @include('components.input-date', [
                             'type' => 'time', 
@@ -51,10 +42,6 @@
                             'name' => 'to_time', 
                             'label' => 'Đến giờ',
                         ])
-                        {{-- <label for="from_time" class="mr-5">Từ giờ</label>
-                        <input type="time" name="from_time" id="from_time">
-                        <label for="to_time" class="mx-5">Đến giờ</label>
-                        <input type="time" name="to_time" id="to_time"> --}}
                     </div>
                     <div class="form-group-row mb-3">
                         @include('components.input-text', [
@@ -71,8 +58,8 @@
                         ])
                         {{-- <label for="content" class="mr-5">Nội dung</label>
                         <textarea type="text" name="content" id="content"></textarea> --}}
-                    </div>  
-                    
+                    </div>
+
                     <div class="btn-group offset-3" role="group">
                         <button type="submit" class="btn btn-light">
                             <i class="fas fa-save"></i>
@@ -86,7 +73,7 @@
                             <i class="fas fa-trash"></i>
                             <span>Xóa</span>
                         </button>
-    
+
                     </div>
 
                 </form>
