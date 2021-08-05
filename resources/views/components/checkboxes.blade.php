feb799119cef44585ac1055441491bbf4e6795f4
quanhoang288 <quanpro288@gmail.com>
2021-08-05 17:44:31 +0700

    update views for update/delete control

--- resources/views/components/checkboxes.blade.php
+++ resources/views/components/checkboxes.blade.php
@@ -22,6 +22,7 @@
             autoload: true,
 
         });
+        
 
 
         tree.on('checkboxChange', function(e, $node, record, state) {
@@ -29,51 +30,48 @@
             if (record.id) {
                
 
-
                 const assigneeName = record.text;
                 const assigneeId = record.id;
 
                 const processMethodId = $('#process_method').val();
                 const processMethodName = $('#process_method option:selected').text();
+                
+                const duplicate = $(`#assignee-table tr[data-process-method-id="${processMethodId}"]#${assigneeId}`);
 
-                if (state === 'checked') {
+                if (state === 'checked' && duplicate.length === 0) {
 
-                    const duplicate = $(`#assignee-table tr[data-process-method-id="${processMethodId}"]#${assigneeId}`);
+                    const assigneeInfo = {
+                        assigneeId: assigneeId,
+                        assigneeName: assigneeName,
+                        processMethodId: processMethodId,
+                        processMethodName: processMethodName,
+                        sms: false,
+                        directReport: false,
+                        deadline: null
 
-                    if (duplicate.length > 0) {
-                        $('#error-modal p').text('Đối tượng đã được chọn với hình thức xử lý này');
-                        $('#error-modal').modal('show');
-                        
-                        tree.uncheck($node);
+                    };
+                    addRowToAssigneeTable('assignee-table', assigneeInfo);
 
-                    } 
-                    else {
-                        const assigneeInfo = {
-                            assigneeId: assigneeId,
-                            assigneeName: assigneeName,
-                            processMethodId: processMethodId,
-                            processMethodName: processMethodName,
-                            sms: false,
-                            directReport: false,
-                            deadline: null
+                    addHiddenInput(assigneeInfo);
 
-                        };
-                        addRowToAssigneeTable('assignee-table', assigneeInfo);
-
-                        addHiddenInput(assigneeInfo);
-
-                        $('#error-modal p').text('');
-                    }
+                    
 
                 }
-                else {
+                else if (state === 'unchecked' && duplicate.length > 0) {
+                    
                     removeFromAssigneeTable('assignee-table', assigneeId, processMethodId);
                     removeHiddenInput(`input[data-process-method-id="${processMethodId}"]#${assigneeId}`);
+
+
+                    
                 }
+                   
+                
 
 
 
             }
+
 
         });
 
