80cdc659ce7c0ddda8940861e617c51f7f3f19db
quanhoang288 <quanpro288@gmail.com>
2021-08-05 10:05:10 +0700

    update register and staff info update validation rules

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -17,29 +17,26 @@ class LoginController extends Controller
 
     public function authenticate(LoginRequest $request)
     {
-    
+
         $email = $request->email;
         $account = Account::where('email', $email)->first();
         $remember = $request->has('remember');
 
         if (!$account->isActive())
             return redirect()->route('login')->withInput()->with('error', 'Tài khoản chưa được kích hoạt bởi quản trị viên');
-        
+
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials, $remember)) {
-            
             return redirect()->route('jobs.index', ['type' => 'handling']);
         }
 
         return redirect()->route('login')->withInput()->with('error', 'Tài khoản hoặc mật khẩu không chính xác');
     }
 
-
     public function logout()
     {
         Auth::logout();
-
         return redirect('login');
     }
 }
--- app/Http/Controllers/StaffInfoController.php
+++ app/Http/Controllers/StaffInfoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\StaffInfo;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StaffInfoController extends Controller
 {
@@ -25,7 +26,25 @@ class StaffInfoController extends Controller
 
     public function update(Request $request, $id)
     {
-        $request->validate(['name' => 'required']);
+        $rules = [
+            'name' => 'required|string|regex:/^[\pL\s]+$/u|max:80',
+            'phone' => 'regex:/^([0-9\s\-\+\(\)]*)$/|size:10'
+        ];
+
+        $messages = [
+            'name.required' => 'Trường tên là bắt buộc',
+            'name.regex' => 'Tên không được phép chứa các ký tự không phải chữ cái',
+            'name.max' => 'Độ dài tên không vượt quá 80 ký tự',
+            'phone.regex' => 'Định dạng số điện thoại không hợp lệ',
+            'phone.size' => 'Số điện thoại phải bao gồm 10 chữ số'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator->errors());
+        }
+
         $staffInfo = StaffInfo::where('staff_id', $id)->with('staff')->first();
         
         try {
@@ -38,10 +57,10 @@ class StaffInfoController extends Controller
             $staffInfo->update($staffInfoUpdates);
             
             $staffInfo->staff->update(['name' => $request->name]);
-            return back()->with('success', 'Cập nhật thông tin thành công');
+            return redirect()->back()->with('success', 'Cập nhật thông tin thành công');
         } 
         catch (Exception $e) {
-            return back()->withInput()->with('error', 'Đã có lỗi xảy ra!');
+            return redirect()->back()->withInput()->with('error', 'Đã có lỗi xảy ra!');
         }
 
 
