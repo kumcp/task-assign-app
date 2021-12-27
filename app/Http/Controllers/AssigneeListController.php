880ee6ff0af24414cb7730f8933465981e4b8ccf
kum_cp <kum_cp@yahoo.com.vn>
2021-12-27 14:48:30 +0700

    modify convention

--- app/Http/Controllers/AssigneeListController.php
+++ app/Http/Controllers/AssigneeListController.php
@@ -19,88 +19,80 @@ class AssigneeListController extends Controller
     const INTERNAL_ERROR = 'INTERNAL_ERROR';
     const JOB_NOT_FOUND = 'JOB_NOT_FOUND';
 
-    
+
     public function index(Request $request)
     {
         $jobs = [];
 
+
+
+        if (!$request->has('jobIds')) {
+            return view('jobs.assignee-list', compact('jobs', 'processMethods'));
+        }
+
+        $jobIds = $request->input('jobIds');
+        $jobs = Job::whereIn('id', $jobIds)->with('assigner')->get();
+
+        if ($jobs->count() == 0) {
+            return redirect()->back()->withErrors(['job_id' => 'Không tìm thấy công việc']);
+        }
+
+        foreach ($jobs as $job) {
+            $job->assigner_name = $job->assigner->name;
+        }
+
         $staffId = Auth::user()->staff_id;
 
-        if ($request->has('jobIds')) {
-            $jobIds = $request->input('jobIds');
+        if ($jobs[0]->assigner_id == $staffId) {
+            $processMethods = ProcessMethod::whereNotIn('name', ['chuyển tiếp'])->get();
+        } else {
 
-
-            $jobs = Job::whereIn('id', $jobIds)->with('assigner')->get();
-
-            if ($jobs->count() == 0) {
-                return redirect()->back()->withErrors(['job_id' => 'Không tìm thấy công việc']);
-            }
-            
-            foreach ($jobs as $job) {
-                $job->assigner_name = $job->assigner->name;
-            }
-
-            if ($jobs[0]->assigner_id == $staffId) {
-                $processMethods = ProcessMethod::whereNotIn('name', ['chuyển tiếp'])->get();
-            }
-            else {
-
-                $jobAssign = JobAssign::where([
-                    'job_id' => $job->id,
-                    'staff_id' => $staffId
-                ])
+            $jobAssign = JobAssign::where([
+                'job_id' => $job->id,
+                'staff_id' => $staffId
+            ])
                 ->with('processMethod')
                 ->first();
 
-                $processMethod = $jobAssign->processMethod;
-                
-                if ($processMethod->assigner) {
+            $processMethod = $jobAssign->processMethod;
 
-                    $processMethods = ProcessMethod::whereNotIn('name', [
-                        $processMethod->name, 
-                    ])->get();
-
-                }
-                else {
-
-                    $processMethods = ProcessMethod::whereIn('name', [
-                        $processMethod->name, 
-                        'chuyển tiếp'
-                    ])->get();
-
-                }
-
-
+            if ($processMethod->assigner) {
+                $processMethods = ProcessMethod::whereNotIn('name', [
+                    $processMethod->name,
+                ])->get();
+            } else {
+                $processMethods = ProcessMethod::whereIn('name', [
+                    $processMethod->name,
+                    'chuyển tiếp'
+                ])->get();
             }
         }
 
         return view('jobs.assignee-list', compact('jobs', 'processMethods'));
-
     }
 
 
-    public function show($jobId) {
+    public function show($jobId)
+    {
         $job = Job::with([
             'assigner',
             'jobAssigns',
             'jobAssigns.processMethod',
             'jobAssigns.assignee',
         ])
-        ->where('id', $jobId)
-        ->first();
+            ->where('id', $jobId)
+            ->first();
 
         $assigneeList = $this->reformat($job);
 
         return response()->json($assigneeList);
-
-        
     }
 
 
-    public function action(Request $request) 
+    public function action(Request $request)
     {
         $action = $request->input('action');
-        return $this->$action($request);            
+        return $this->$action($request);
     }
 
 
@@ -110,15 +102,13 @@ class AssigneeListController extends Controller
 
         if ($request->has('old_job_assigns')) {
             $result = $this->updateOldJobAssigns($request);
-            
+
             if ($result['status_code'] == self::INTERNAL_ERROR) {
                 return redirect()->back()->with('error', $result['message']);
             }
-        }
-
-        else {        
+        } else {
             $validator = $this->makeValidator($request);
-            
+
             if ($validator->fails()) {
                 return redirect()->back()->withErrors($validator->errors());
             }
@@ -134,7 +124,6 @@ class AssigneeListController extends Controller
             return redirect()->back()->with('error', $result['message']);
         }
         return redirect()->back()->with('success', $result['message']);
-    
     }
 
     public function delete($request)
@@ -150,7 +139,6 @@ class AssigneeListController extends Controller
 
             JobAssign::whereIn('id', $jobAssignIds)->delete();
             return redirect()->back()->with('success', 'Xóa đối tượng xử lý thành công');
-
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return redirect()->back()->with('error', 'Đã có lỗi xảy ra');
@@ -159,7 +147,7 @@ class AssigneeListController extends Controller
 
     private function updateOldJobAssigns($request)
     {
-        $oldJobAssignUpdates = array_map(function($jobAssign) {
+        $oldJobAssignUpdates = array_map(function ($jobAssign) {
             return json_decode($jobAssign, true);
         }, $request->input('old_job_assigns'));
 
@@ -167,7 +155,7 @@ class AssigneeListController extends Controller
 
             foreach ($oldJobAssignUpdates as $update) {
                 $jobAssignId = $update['id'];
-    
+
                 $oldJobAssign = JobAssign::find($jobAssignId);
                 $oldJobAssign->update([
                     'sms' => $update['sms'],
@@ -179,7 +167,6 @@ class AssigneeListController extends Controller
                 'status_code' => self::SAVE_SUCCESSFUL,
                 'message' => 'Cập nhật giao xử lý thành công'
             ];
-
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return [
@@ -204,19 +191,19 @@ class AssigneeListController extends Controller
         return Validator::make($request->all(), $rules, $messages);
     }
 
-    private function createNewJobAssigns($request) 
+    private function createNewJobAssigns($request)
     {
         $jobAssigns = $request->input('job_assigns');
-        $jobAssigns = array_map(function($item) {
+        $jobAssigns = array_map(function ($item) {
             return json_decode($item, true);
         }, $jobAssigns);
 
         $jobIds = $request->input('job_ids');
         $staffId = Auth::user()->staff_id;
 
-        try{
+        try {
             $jobs = Job::whereIn('id', $jobIds)->with('jobAssigns')->get();
-            
+
             if ($jobs->count() == 0) {
                 return [
                     'status_code' => self::JOB_NOT_FOUND,
@@ -225,48 +212,44 @@ class AssigneeListController extends Controller
             }
 
             $forwardProcessMethod = ProcessMethod::where('name', 'chuyển tiếp')->first();
-            $forwardJobAssigns = array_filter($jobAssigns, function($jobAssign) use ($forwardProcessMethod) {
+            $forwardJobAssigns = array_filter($jobAssigns, function ($jobAssign) use ($forwardProcessMethod) {
                 return intval($jobAssign['process_method_id']) == $forwardProcessMethod->id;
             });
 
-            $otherJobAssigns = array_filter($jobAssigns, function($jobAssign) use ($forwardProcessMethod) {
+            $otherJobAssigns = array_filter($jobAssigns, function ($jobAssign) use ($forwardProcessMethod) {
                 return intval($jobAssign['process_method_id']) != $forwardProcessMethod->id;
             });
 
             foreach ($jobs as $job) {
-                
-                if ($job->assigner_id == $staffId) {                            
+
+                if ($job->assigner_id == $staffId) {
                     $job->jobAssigns()->createMany($otherJobAssigns);
-                }
-                else {
+                } else {
                     $currentJobAssign = JobAssign::where([
                         'job_id' => $job->id,
                         'staff_id' => $staffId
                     ])->first();
-    
-                    $mappedForwardJobAssigns = array_map(function($jobAssign) use ($currentJobAssign) {
-                        
+
+                    $mappedForwardJobAssigns = array_map(function ($jobAssign) use ($currentJobAssign) {
+
                         $jobAssign['parent_id'] = $currentJobAssign->id;
                         $jobAssign['process_method_id'] = $currentJobAssign->process_method_id;
 
                         return $jobAssign;
-
                     }, $forwardJobAssigns);
-    
-                    $mappedOtherJobAssigns = array_map(function($jobAssign) use ($currentJobAssign) {
-                        
+
+                    $mappedOtherJobAssigns = array_map(function ($jobAssign) use ($currentJobAssign) {
+
                         $jobAssign['parent_id'] = $currentJobAssign->id;
                         $jobAssign['is_additional'] = true;
-                        
-                        return $jobAssign;
 
+                        return $jobAssign;
                     }, $otherJobAssigns);
-    
+
                     $newJobAssigns = array_merge($mappedForwardJobAssigns, $mappedOtherJobAssigns);
 
 
                     $job->jobAssigns()->createMany($newJobAssigns);
-    
                 }
             }
 
@@ -274,18 +257,17 @@ class AssigneeListController extends Controller
                 'status_code' => self::SAVE_SUCCESSFUL,
                 'message' => 'Giao xử lý thành công'
             ];
-
         } catch (Exception $e) {
             Log::error($e->getMessage());
             return [
                 'status_code' => self::INTERNAL_ERROR,
-                'message' => 'Đã có lỗi xảy ra' 
+                'message' => 'Đã có lỗi xảy ra'
             ];
         }
     }
 
 
-    private function reformat ($job) 
+    private function reformat($job)
     {
         $reformat = [];
 
@@ -299,14 +281,12 @@ class AssigneeListController extends Controller
                 'assignee' => $jobAssign->assignee->name,
                 'process_method' => $jobAssign->processMethod->name,
                 'history' => null, //TODO: get forward histories
-                'status' => __('job-assign.status.' . $jobAssign->status) 
+                'status' => __('job-assign.status.' . $jobAssign->status)
             ];
 
             $reformat[] = $reformatObj;
-        } 
+        }
 
         return $reformat;
-
-
     }
 }
