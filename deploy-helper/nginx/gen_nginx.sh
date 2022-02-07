6516bdeb01fd38b57f133a865763c636b54ada22
kum_cp <kum_cp@yahoo.com.vn>
2022-02-07 07:47:47 +0700

    add separate docker for different env

--- deploy-helper/nginx/gen_nginx.sh
+++ deploy-helper/nginx/gen_nginx.sh
@@ -3,5 +3,7 @@
 echo "Staring nginx"
 envsubst '${NGINX_HOST} ${NGINX_PORT}' < /etc/nginx/helper/nginx-config-docker.template > /etc/nginx/conf.d/default.conf
 
-nginx -g 'daemon off;'
+
+
+# nginx -g 'daemon off;'
 
