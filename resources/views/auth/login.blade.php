85261b4bd39e94c95aca447d83aafaee94759b71
kum_cp <kum_cp@yahoo.com.vn>
2022-05-24 20:15:56 +0700

    update project v4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Đăng nhập</div>
+                    <div class="card-header">Login</div>
 
                     @include('components.flash-message')
 
